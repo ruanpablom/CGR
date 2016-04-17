d6630be5f01e10ac267c768989100e98adffb5b4
Udesc <ruan@udesc>
2016-04-16 22:01:22 -0300

    Rain

--- rain.c
+++ rain.c
@@ -1,4 +1,4 @@
-// gcc fireworks_glut.c -lglut -lGL -lGLU -lm -o fireworks_glut
+// gcc rain.c -lglut -lGL -lGLU -lm -o rain
 #include <GL/glut.h>  // Header File For The GLUT Library 
 #include <GL/gl.h>	  // Header File For The OpenGL32 Library
 #include <GL/glu.h>	  // Header File For The GLu32 Library
